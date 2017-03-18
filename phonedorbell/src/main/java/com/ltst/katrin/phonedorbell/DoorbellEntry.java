a59a448071be40b944e2d32eafbc89251e0b2af3
Ekaterina Yakimova <e.yakimova@ltst.ru>
2017-03-18 14:54:08 +0600

    phoneBell

---
+++ phonedorbell/src/main/java/com/ltst/katrin/phonedorbell/DoorbellEntry.java
@@ -0,0 +1,30 @@
+package com.ltst.katrin.phonedorbell;
+
+import java.util.Map;
+
+public class DoorbellEntry {
+    Long timestamp;
+    String image;
+    Map<String, Float> annotations;
+
+    public DoorbellEntry() {
+    }
+
+    public DoorbellEntry(Long timestamp, String image, Map<String, Float> annotations) {
+        this.timestamp = timestamp;
+        this.image = image;
+        this.annotations = annotations;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Map<String, Float> getAnnotations() {
+        return annotations;
+    }
+}
